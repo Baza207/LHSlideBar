8d4c8aafc9f13c9bf44c10fde24d19f16d83b7fe
James Barrow <baza207@me.com>
2013-07-12 19:10:55 +0200

    Initial Commit

---
+++ LHSlideBar/LHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LHAppDelegate.h
+//  LHSlideBar
+//
+//  Created by James Barrow on 12/07/2013.
+//  Copyright (c) 2013 Pig on a Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LHSlideBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LHSlideBar
+//
+//  Created by James Barrow on 12/07/2013.
+//  Copyright (c) 2013 Pig on a Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LHAppDelegate class]));
+    }
+}
